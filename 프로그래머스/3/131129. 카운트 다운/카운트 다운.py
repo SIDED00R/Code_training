248c5a9f3704ba894e6478a352632d07ef8135d6
Jaeho Lee <leedominico@naver.com>
2024-01-03 07:24:09 +0900

    [level 3] Title: 카운트 다운, Time: 0.19 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/131129. 카운트 다운/카운트 다운.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+
+def solution(target):
+    dic = defaultdict(int)
+    for num in range(1, 21):
+        for mul in range(1, 4):
+            if mul == 1:
+                dic[num * mul] = 1
+            else:
+                dic[num * mul] = 0
+    dic[50] = 1
+    
+    dp = [[2000, 0] for _ in range(target + 1)]
+    dp[0] = [0, 0]
+    for idx in range(target + 1):
+        now = dp[idx]
+        for key, value in dic.items():
+            if idx + key <= target:
+                check = dp[idx + key]
+                if now[0] + 1 < check[0] or (now[0] + 1 == check[0] and now[1] + value > check[1]):
+                    dp[idx + key] = [now[0] + 1, now[1] + value]
+                    
+    return dp[target]
