067456df2e87c36832d86f80efb243f9cda25331
Jaeho Lee <leedominico@naver.com>
2024-01-03 07:26:29 +0900

    [level 3] Title: 카운트 다운, Time: 0.20 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/131129. 카운트 다운/카운트 다운.py
+++ 프로그래머스/3/131129. 카운트 다운/카운트 다운.py
@@ -1,6 +1,10 @@
 from collections import defaultdict
 
 def solution(target):
+    count = 0
+    while target > 600:
+        target -= 60
+        count += 1
     dic = defaultdict(int)
     for num in range(1, 21):
         for mul in range(1, 4):
@@ -19,5 +23,5 @@ def solution(target):
                 check = dp[idx + key]
                 if now[0] + 1 < check[0] or (now[0] + 1 == check[0] and now[1] + value > check[1]):
                     dp[idx + key] = [now[0] + 1, now[1] + value]
-                    
+    dp[target][0] += count               
     return dp[target]
