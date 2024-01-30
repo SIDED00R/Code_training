5919515a3a44b8af8f800b8c79a0d6d5f5ac9d86
Jaeho Lee <leedominico@naver.com>
2024-01-30 20:59:53 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.py
@@ -0,0 +1,43 @@
+import sys
+input = sys.stdin.readline
+
+def find_idx(stack, num):
+    find = False
+    start = 0
+    end = len(stack) - 1
+    while not find:
+        mid = (start + end) // 2
+        if stack[mid] < num:
+            start = mid
+        else:
+            end = mid
+        if start + 1 >= end:
+            return end
+
+N = int(input())
+A = list(map(int, input().rstrip('\n').split()))
+
+stack = [-1000000001]
+count_list = []
+max_num = 0
+
+for num in A:
+    if stack[-1] < num:
+        stack.append(num)
+        count_list.append([num, len(stack) - 1])
+        max_num = len(stack) - 1
+    else:
+        idx = find_idx(stack, num)
+        stack[idx] = num
+        count_list.append([num, idx])
+       
+answer = []
+print(max_num)
+for case in count_list[::-1]:
+    num, idx = case
+    if idx == max_num:
+        max_num -= 1
+        answer.append(num)
+for ans in answer[::-1]:
+    print(ans, end = " ")
+
