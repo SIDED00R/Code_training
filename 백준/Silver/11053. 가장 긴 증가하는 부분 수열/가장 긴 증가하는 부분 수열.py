5a8c87d1a00f72f0e0de54d454c58d79a6f02481
Jaeho Lee <leedominico@naver.com>
2024-01-30 21:01:26 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+
+def find_idx(stack, num):
+    find = False
+    start = 0
+    end = len(stack) - 1
+    while not find:
+        mid = (start + end) // 2
+        if stack[mid] < num:
+            start = mid
+        else:
+            end = mid
+        if start + 1 >= end:
+            return end
+
+N = int(input())
+A = list(map(int, input().rstrip('\n').split()))
+
+stack = [-1000000001]
+count_list = []
+max_num = 0
+
+for num in A:
+    if stack[-1] < num:
+        stack.append(num)
+        count_list.append([num, len(stack) - 1])
+        max_num = len(stack) - 1
+    else:
+        idx = find_idx(stack, num)
+        stack[idx] = num
+        count_list.append([num, idx])
+
+print(max_num)
+
