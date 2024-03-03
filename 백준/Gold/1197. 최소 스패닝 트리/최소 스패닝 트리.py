53680728d07ed3d6f5c8f2016b8e91fe8efb5560
Jaeho Lee <leedominico@naver.com>
2024-03-04 07:50:09 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 396 ms, Memory: 53428 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+import heapq
+
+def find(node):
+    if parents_node[node] != node:
+        parents_node[node] = find(parents_node[node])
+    return parents_node[node]
+
+def union(first, second):
+    first_root = find(first)
+    second_root = find(second)
+    if first_root < second_root:
+        parents_node[second_root] = first_root
+    else:
+        parents_node[first_root] = second_root
+
+V, E = map(int, input().rstrip('\n').split())
+parents_node = [i for i in range(V + 1)]
+nodes = []
+for _ in range(E):
+    first, second, weight = map(int, input().rstrip('\n').split())
+    heapq.heappush(nodes, [weight, first, second])
+
+answer = 0    
+count = 0
+while count < V - 1:
+    now_weight, now_start, now_end = heapq.heappop(nodes)
+    if find(now_start) != find(now_end):  
+        union(now_start, now_end)  
+        answer += now_weight
+        count += 1
+               
+print(answer)
