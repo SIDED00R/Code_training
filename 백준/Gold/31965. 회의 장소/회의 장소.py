b85fd5dcf51dd56c87ac382b454d300de857b14c
Jaeho Lee <leedominico@naver.com>
2024-08-18 08:35:54 +0900

    [Gold II] Title: 회의 장소, Time: 248 ms, Memory: 111880 KB, Score: 65 point -BaekjoonHub

--- 백준/Gold/31965. 회의 장소/회의 장소.py
+++ 백준/Gold/31965. 회의 장소/회의 장소.py
@@ -1,36 +1,32 @@
-def b_search(line, num):
-    start = 0
-    end = len(line) - 1
-    while start <= end:
-        mid = (start + end) // 2
-        if line[mid] <= num:
-            start = mid + 1
+from bisect import bisect_left, bisect_right
+
+def get_cost(X, P, i, j, k):
+    left_cost = (k - i + 1) * X[k] - (P[k] - P[i - 1])
+    right_cost = (P[j] - P[k - 1]) - (j - k + 1) * X[k]
+    return left_cost + right_cost
+
+def main():
+    n, q = map(int, input().split())
+    X = [0] * (n + 1)
+    P = [0] * (n + 1)
+    
+    line = list(map(int, input().split()))
+    for i in range(1, n + 1):
+        X[i] = line[i - 1]
+        P[i] = P[i - 1] + X[i]
+    
+    for _ in range(q):
+        l, r = map(int, input().split())
+        
+        lo = bisect_left(X, l, 1, n + 1)
+        hi = bisect_right(X, r, 1, n + 1) - 1
+        
+        if lo >= hi:
+            print(0)
         else:
-            end = mid - 1
-    return start
+            max_stress = max(get_cost(X, P, lo, hi, lo), get_cost(X, P, lo, hi, hi))
+            min_stress = get_cost(X, P, lo, hi, (lo + hi) // 2)
+            print(max_stress - min_stress)
 
-n, q = map(int, input().split())
-line = list(map(int, input().split()))
-add_list = [0]
-for num in line:
-    add_list.append(num + add_list[-1])
-    
-
-for _ in range(q):
-    l, r = map(int, input().split())
-    s_idx = b_search(line, l)
-    e_idx = b_search(line, r)
-    if line[s_idx - 1] != l:
-        s_idx += 1
-    if e_idx - s_idx < 1:
-        print(0)
-    else:
-        total_weight = add_list[e_idx] - add_list[s_idx - 1]
-        middle = total_weight / (e_idx - s_idx + 1)
-        m_idx = b_search(line, middle)
-        if abs(line[m_idx] - middle) < abs(line[m_idx - 1] - middle):
-            m_idx += 1
-        min_value = (add_list[e_idx] - add_list[m_idx]) - line[m_idx - 1] * (e_idx - m_idx) + line[m_idx - 1] * (m_idx - s_idx) - (add_list[m_idx - 1] - add_list[s_idx - 1])
-        max_value = max(add_list[e_idx] - add_list[s_idx] - line[s_idx - 1] * (e_idx - s_idx), line[e_idx - 1] * (e_idx - s_idx) - (add_list[e_idx - 1] - add_list[s_idx - 1]))
-        print(max_value - min_value)
-    
+if __name__ == "__main__":
+    main()
