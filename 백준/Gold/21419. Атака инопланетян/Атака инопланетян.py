2143c58d23e609fccb1169bb6e12bed8bf0737a0
Jaeho Lee <leedominico@naver.com>
2025-02-22 13:07:38 +0900

    [Gold V] Title: Атака инопланетян, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/21419. Атака инопланетян/Атака инопланетян.py
+++ 백준/Gold/21419. Атака инопланетян/Атака инопланетян.py
@@ -1,49 +1,20 @@
-import sys
-from dataclasses import dataclass
+n = int(input())
+tri = []
+max_x = -1000000
+max_y = -1000000
+for _ in range(n):
+    x, y, s = map(int, input().split())
+    max_x = max(max_x, x)
+    max_y = max(max_y, y)
+    tri.append([x, y, s])
 
-def read_ints():
-    return map(int, sys.stdin.readline().split())
+short_x = 10**20
 
-@dataclass
-class Triangle:
-    x: int
-    y: int
-    side: int
-    
-    def in_triangle(self, px: int, py: int) -> bool:
-        return px >= self.x and py >= self.y and (py + px) <= (self.x + self.side + self.y)
-    
-    @staticmethod
-    def read():
-        x, y, s = read_ints()
-        return Triangle(2 * x, 2 * y, 2 * s)
+for now_case in tri:
+    x, y, s = now_case
+    short_x = min(short_x, x + y + s - max_y - max_x)
 
-def intersect(a: Triangle, b: Triangle) -> Triangle:
-    x = max(a.x, b.x)
-    y = max(a.y, b.y)
-    
-    if not a.in_triangle(x, y) or not b.in_triangle(x, y):
-        return Triangle(x, y, 0)
-    
-    low, high = 0, int(1e9)
-    while high - low > 1:
-        mid = (low + high) // 2
-        if not (a.in_triangle(x + mid, y) and a.in_triangle(x, y + mid) and 
-                b.in_triangle(x + mid, y) and b.in_triangle(x, y + mid)):
-            high = mid
-        else:
-            low = mid
-    
-    return Triangle(x, y, low)
-
-def main():
-    n = int(sys.stdin.readline())
-    tr = Triangle.read()
-    
-    for _ in range(n - 1):
-        tr = intersect(tr, Triangle.read())
-    
-    print(f"{(tr.side * tr.side) / 8:.3f}")
-
-if __name__ == "__main__":
-    main()
+if short_x > 0:
+    print(f"{(short_x)**2/2:.3f}")
+else:
+    print(0)
