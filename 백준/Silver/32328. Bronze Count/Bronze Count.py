fda270025e87c298c7b87a0ba36028bfd96874e5
Jaeho Lee <leedominico@naver.com>
2025-03-26 08:55:29 +0900

    [Silver IV] Title: Bronze Count, Time: 224 ms, Memory: 112788 KB, Score: 15 point -BaekjoonHub

--- 백준/Silver/32328. Bronze Count/Bronze Count.py
+++ 백준/Silver/32328. Bronze Count/Bronze Count.py
@@ -2,19 +2,12 @@ from collections import defaultdict
 import heapq
 
 dic = defaultdict(int)
-stack = []
+stack = set()
 n = int(input())
 for _ in range(n):
     inp = int(input())
-    heapq.heappush(stack, -inp)
+    stack.add(inp)
     dic[inp] += 1
 
-g = stack[0]
-while stack[0] == g:
-    heapq.heappop(stack)
-
-s = stack[0]
-while stack[0] == s:
-    heapq.heappop(stack)
-
-print(-stack[0], dic[-stack[0]])
+stack = sorted(list(stack))
+print(stack[-3], dic[stack[-3]])
