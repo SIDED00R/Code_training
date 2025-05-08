0644c76dce229515e32284b24f8aa86e17a3c50a
Jaeho Lee <leedominico@naver.com>
2025-05-08 20:38:51 +0900

    [Bronze I] Title: 초성 변환, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/12090. 초성 변환/초성 변환.py
@@ -0,0 +1,17 @@
+first = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+second = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ']
+last = [' ', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+
+def trans(korean_word):
+    r_lst = []
+    for w in list(korean_word.strip()):
+        ch1 = (ord(w) - ord('가')) // 588
+        ch2 = ((ord(w) - ord('가')) - (588 * ch1)) // 28
+        ch3 = (ord(w) - ord('가')) - (588 * ch1) - 28 * ch2
+        r_lst.append([first[ch1], second[ch2], last[ch3]])
+    return r_lst
+
+letter = input()
+letter_list = trans(letter)
+for now_word in letter_list:
+    print(now_word[0], end = '')
