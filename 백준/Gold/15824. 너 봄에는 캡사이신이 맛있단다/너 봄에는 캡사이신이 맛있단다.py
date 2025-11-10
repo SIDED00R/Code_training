31803c9723d04ad722bb034554757ee79a83b92e
Jaeho Lee <leedominico@naver.com>
2025-11-11 08:05:38 +0900

    [Gold II] Title: 너 봄에는 캡사이신이 맛있단다, Time: 320 ms, Memory: 184600 KB, Score: 250 point -BaekjoonHub

--- 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.py
+++ 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.py
@@ -5,7 +5,7 @@ n = int(input())
 line = sorted(list(map(int, input().split())))
 
 dp = [0, 0]
-
+power_matrix = [pow(2, i, 1000000007) for i in range(1, n)]
 for i in range(1, n):
-    dp.append((dp[-1] * 2 + (pow(2, i) - 1) * (line[i] - line[i - 1])) % 1000000007)
+    dp.append((dp[-1] * 2 + (power_matrix[i - 1] - 1) * (line[i] - line[i - 1])) % 1000000007)
 print(sum(dp) % 1000000007)
