482bd47b5d92453812d49fb04c0a5502b55037e3
Jaeho Lee <leedominico@naver.com>
2025-12-13 08:41:41 +0900

    [Silver V] Title: CD, Time: 1492 ms, Memory: 231320 KB -BaekjoonHub

---
+++ 백준/Silver/4158. CD/CD.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+while True:
+    n, m = map(int, input().split())
+    if n == 0 and m == 0:
+        break
+    dic = {}
+    for _ in range(n):
+        dic[int(input())] = 1
+    
+    answer = 0
+    for _ in range(m):
+        if int(input()) in dic:
+            answer += 1
+    print(answer)
