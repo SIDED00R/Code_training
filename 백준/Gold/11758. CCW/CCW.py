65a24867bb316a567d53c93618e09fb702e110f2
Jaeho Lee <leedominico@naver.com>
2026-02-23 06:03:31 +0900

    [Gold V] Title: CCW, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/11758. CCW/CCW.py
@@ -0,0 +1,10 @@
+x1, y1 = map(int, input().split())
+x2, y2 = map(int, input().split())
+x3, y3 = map(int, input().split())
+check = (x2 - x1) * (y3 - y1) - (y2 - y1) * (x3 - x1)
+if check > 0:
+    print(1)
+elif check < 0:
+    print(-1)
+else:
+    print(0)
