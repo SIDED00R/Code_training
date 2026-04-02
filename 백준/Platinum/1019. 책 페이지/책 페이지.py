4c2e105bfc4b4ac1cd2efd1b2890f7ea157549af
Jaeho Lee <leedominico@naver.com>
2026-04-03 08:06:19 +0900

    [Platinum V] Title: 책 페이지, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Platinum/1019. 책 페이지/책 페이지.py
@@ -0,0 +1,23 @@
+def find(num, dic):
+    length = len(num)
+    if length == 1:
+        for num in range(int(num) + 1):
+            dic[num] += 1
+        return dic
+    first_num = int(num[0])
+    for f_n in range(first_num):
+        dic[f_n] += 10 ** (length - 1)
+        for i in range(10):
+            dic[i] += (length - 1) * 10 ** (length - 2)
+    dic[first_num] += int(num[1:]) + 1
+    return dic
+
+n = input()
+dic = {i:0 for i in range(10)}
+dic[0] -= (10 ** len(n) - 1) // 9
+while n:
+    dic = find(n, dic)
+    n = n[1:]
+
+for i in range(10):
+    print(dic[i])
