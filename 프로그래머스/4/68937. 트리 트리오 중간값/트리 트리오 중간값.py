92ca0b391eda9138b0a4dede3a39b11906874bba
Jaeho Lee <leedominico@naver.com>
2026-08-02 09:54:28 +0900

    [level 4] Title: 트리 트리오 중간값, Time: 628.33 ms, Memory: 139 MB -BaekjoonHub

--- 프로그래머스/4/68937. 트리 트리오 중간값/트리 트리오 중간값.py
+++ 프로그래머스/4/68937. 트리 트리오 중간값/트리 트리오 중간값.py
@@ -1,61 +1,61 @@
+from collections import defaultdict
+import sys
+sys.setrecursionlimit(10000000)
+
+answer = 0
+count = 0
+
+def find(num, route, visited):
+    global answer
+    global count
+
+    branches = [(0, 1)]                    # (깊이, 그 깊이의 노드 수). 자기 자신
+    for next_node in route[num]:
+        if not visited[next_node]:
+            visited[next_node] = True
+            next_depth, next_cnt = find(next_node, route, visited)
+            branches.append((next_depth + 1, next_cnt))
+
+    branches.sort(reverse=True)
+    first_max = branches[0][0]
+    second_max = branches[1][0] if len(branches) > 1 else -1
+
+    if second_max >= 0:                    # 가지가 2개 이상일 때만 경로가 생김
+        if first_max == second_max:        # 같은 깊이끼리 짝짓기
+            s = sum(m for d, m in branches if d == first_max)
+            sq = sum(m * m for d, m in branches if d == first_max)
+            paths = (s * s - sq) // 2
+        else:
+            a = sum(m for d, m in branches if d == first_max)
+            b = sum(m for d, m in branches if d == second_max)
+            paths = a * b
+
+        if answer < first_max + second_max:
+            answer = first_max + second_max
+            count = paths
+        elif answer == first_max + second_max:
+            count += paths
+
+    return first_max, sum(m for d, m in branches if d == first_max)
+
+
 def solution(n, edges):
-    adj = [[] for _ in range(n + 1)]
-    for a, b in edges:
-        adj[a].append(b)
-        adj[b].append(a)
+    global answer
+    global count
+    answer = 0
+    count = 0
 
-    # 1) BFS 순서 + 부모
-    parent = [0] * (n + 1)
-    order = []
-    visited = [False] * (n + 1)
+    route = defaultdict(list)
+    for edge in edges:
+        a, b = edge
+        route[a].append(b)
+        route[b].append(a)
+
+    visited = [False for _ in range(n + 1)]
     visited[1] = True
-    stack = [1]
-    while stack:
-        v = stack.pop()
-        order.append(v)
-        for w in adj[v]:
-            if not visited[w]:
-                visited[w] = True
-                parent[w] = v
-                stack.append(w)
+    find(1, route, visited)
 
-    # 2) down[v] = v의 서브트리 아래 방향 최대 깊이 (역순 순회 = post-order)
-    down = [0] * (n + 1)
-    for v in reversed(order):
-        for w in adj[v]:
-            if w != parent[v] and down[w] + 1 > down[v]:
-                down[v] = down[w] + 1
-
-    # 3) up[v] = v에서 부모 방향으로의 최대 깊이 (정순 순회)
-    up = [0] * (n + 1)
-    ans = 0
-    for v in order:
-        b1 = b2 = 0
-        arg1 = -1
-        for w in adj[v]:
-            if w == parent[v]:
-                continue
-            c = down[w] + 1
-            if c > b1:
-                b2, b1, arg1 = b1, c, w
-            elif c > b2:
-                b2 = c
-        for w in adj[v]:
-            if w == parent[v]:
-                continue
-            sib = b2 if w == arg1 else b1   # w 제외한 형제 중 최대
-            up[w] = 1 + max(up[v], sib)
-
-        # 4) v에서의 모든 가지 깊이 상위 3개 → d1 + d3
-        d1 = d2 = d3 = 0
-        cnt = 0
-        for w in adj[v]:
-            c = up[v] if w == parent[v] else down[w] + 1
-            cnt += 1
-            if c > d1:   d3, d2, d1 = d2, d1, c
-            elif c > d2: d3, d2 = d2, c
-            elif c > d3: d3 = c
-        if cnt >= 2:          # 가지 2개 미만이면 median vertex가 될 수 없음
-            ans = max(ans, d1 + d3)
-
-    return ans
+    if count >= 2:
+        return answer
+    else:
+        return answer - 1
