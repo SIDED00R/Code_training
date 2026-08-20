45e04d97dea11c44c62d12854d7c2f0675e539f6
Jaeho Lee <leedominico@naver.com>
2026-08-20 17:05:37 +0900

    [Codeforces] 126B. Password

--- Codeforces/126B. Password/126B.py
+++ Codeforces/126B. Password/126B.py
@@ -1,21 +1,22 @@
-import sys
-input = sys.stdin.readline
+s = input()
+n = len(s)
+pi = [0] * n
+k = 0
+for i in range(1, n):
+    while k > 0 and s[i] != s[k]:
+        k = pi[k - 1]
+    if s[i] == s[k]:
+        k += 1
+    pi[i] = k
 
-s = input().rstrip()
-able = []
-
-for idx in range(1, len(s) + 1):
-    if s[:idx] == s[-idx:]:
-        able.append(s[-idx:])
-
-find = False
-for now_case in able[::-1]:
-    if now_case in s[1:-1]:
-        find = True
-        answer = now_case
-        break
-
-if find:
-    print(answer)
+if n < 3:
+    ans = 0
 else:
+    m = max(pi[:-1])
+    ans = pi[-1]
+    while ans > m:
+        ans = pi[ans - 1]
+if ans == 0:
     print("Just a legend")
+else:
+    print(s[-ans:])
