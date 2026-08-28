389eb52eaf72cfcf89549c6b77f3d35a3cc9fb35
Jaeho Lee <leedominico@naver.com>
2026-08-28 10:29:15 +0900

    [Codeforces] 1537E1. Erase and Extend (Easy Version)

--- Codeforces/1537E1. Erase and Extend (Easy Version)/1537E1.py
+++ Codeforces/1537E1. Erase and Extend (Easy Version)/1537E1.py
@@ -1,17 +1,16 @@
 import sys
 input = sys.stdin.readline
-
+ 
 n, k = map(int, input().split())
 word = input().rstrip()
-answer = word
+length = len(word)
+mul = k // length
+answer = word * (mul + 1)
+answer = answer[:k]
 for idx in range(1, n):
 	front = word[:idx]
-	back = word[idx:]
-	if front < back:
-		answer = front
-		break
-		
-length = len(answer)
-mul = k // length
-answer *= mul + 1
-print(answer[:k])
+	length = len(front)
+	mul = k // length
+	front *= mul + 1
+	answer = min(answer, front[:k])
+print(answer)
